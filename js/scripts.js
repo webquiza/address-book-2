fc25db93dbd3314bf566ccfcfb6ac6b9ed03e6e8
Carlos Urquiza <webquiza@gmail.com>
2021-07-06 19:31:36 -0700

    complete practice project

--- js/scripts.js
+++ js/scripts.js
@@ -33,12 +33,37 @@ AddressBook.prototype.deleteContact = function(id) {
 
 // Business Logic for Contacts
 
-function Contact(firstName, LastName, phoneNumber) {
+function Contact(firstName, lastName, phoneNumber) {
   this.firstName = firstName;
-  this.LastName = LastName;
+  this.lastName = lastName;
   this.phoneNumber = phoneNumber;
 }
 
 Contact.prototype.fullName = function() {
   return this.firstName + " " + this.lastName;
 };
+
+// User Interface Logic ---------
+let addressBook = new AddressBook();
+
+function displayContactDetails(addressBookToDisplay) {
+  let contactsList = $("ul#contacts");
+  let htmlForContactInfo = "";
+  Object.keys(addressBookToDisplay.contacts).forEach(function(key) {
+    const contact = addressBookToDisplay.findContact(key);
+    htmlForContactInfo += "<li id=" + contact.id + ">" + contact.firstName + " " + contact.lastName + "</li>";
+  });
+  contactsList.html(htmlForContactInfo);
+}
+
+$(document).ready(function() {
+  $("form#new-contact").submit(function(event) {
+    event.preventDefault();
+    const inputtedFirstName = $("input#new-first-name").val();
+    const inputtedLastName = $("input#new-last-name").val();
+    const inputtedPhoneNumber = $("input#new-phone-number").val();
+    let newContact = new Contact(inputtedFirstName, inputtedLastName, inputtedPhoneNumber);
+    addressBook.addContact(newContact);
+    displayContactDetails(addressBook);
+  });
+});
